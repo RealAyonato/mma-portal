cf650c80e47ab597701f9ff2378f2e835abea5b7
RealAyonato <ayonato.art@gmail.com>
2026-07-03 21:25:42 +0100

    Security: Move Firebase keys to .env file

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,20 +1,14 @@
-// src/firebaseConfig.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// ضع هنا البيانات الخاصة بمشروعك التي حصلت عليها من موقع Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyD7x03ywHdN8D80vkZFZsvwyjvHOA9ioK4",
-  authDomain: "mounir-mma.firebaseapp.com",
-  projectId: "mounir-mma",
-  storageBucket: "mounir-mma.firebasestorage.app",
-  messagingSenderId: "10243811164",
-  appId: "1:10243811164:web:796c3c20d759b76d808399",
-  
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
-// تهيئة Firebase
 const app = initializeApp(firebaseConfig);
-
-// ⚠️ تأكد من وجود كلمة export في هذا السطر تحديداً لكي يراها المترجم:
 export const db = getFirestore(app);
